ea12ed0232f8ec69d2b09333efaa099b959c9772
Najmanager <106113097+Najmanager@users.noreply.github.com>
2022-09-24 18:45:56 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const ejs = require('ejs')
+
+const app = express()
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
